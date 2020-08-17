b3357d895770fcbfc1baeea0576d2d7fd8279283
gfmacaraeg <gianfranco.macaraeg@my.tccd.edu>
2020-08-17 16:14:52 -0500

    First React with JSX

---
+++ React/First React with JSX/app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <li>
+        <ul>Learn React</ul>
+        <ul>Climb Mt. Everest</ul>
+        <ul>Run a marathon</ul>
+        <ul>Feed the dogs.</ul>
+      </li>
+    </div>
+  );
+}
+
+export default App;
