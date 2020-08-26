15a5fb81b20748f11e9248de5ca046febdf32464
gfmacaraeg <gianfranco.macaraeg@my.tccd.edu>
2020-08-26 10:48:38 -0500

    Prop it Up Assignment

---
+++ React/PropItUp/app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './Components/PersonCard'
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName= "Jane" lastName = "Doe" age={45} hairColor = "Black"></PersonCard>
+      <PersonCard firstName= "John" lastName = "Smith" age={88} hairColor = "Brown"></PersonCard>
+      <PersonCard firstName= "Millard" lastName = "Fillmore" age={45} hairColor = "Brown"></PersonCard>
+      <PersonCard firstName= "Maria" lastName = "Smith" age={62} hairColor = "Brown"></PersonCard>
+    </div>
+  );
+}
+
+export default App;
